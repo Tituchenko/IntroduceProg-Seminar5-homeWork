eb310631df25540b97d3b9ecd0fc911940996ba9
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-29 22:46:05 +0300

    Ex 36 Sum of Odd index elements - solved

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 //Напишите программу, которая покажет количество чётных чисел в массиве.
 
 //[345, 897, 568, 234] -> 2
-
+/*
 int[] arr=GetArray (7,100,999);
 
 printArray(arr);
@@ -18,6 +18,42 @@ int getEvenElements (int[] arr)
     }
     return count;
 }
+*/
+
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+
+//[-4, -6, 89, 6] -> 0
+
+int[] arr=GetArray (7,-20,20);
+printArray(arr);
+Console.WriteLine ($" -> {getSumOfOddIndex(arr)}");
+int getSumOfOddIndex (int[] arr)
+{
+    int count=0;
+    for (int i=1;i<arr.Length;i+=2){
+       count+=arr[i]; // без всяких if и лишних итераций, должны же мы беречь процессорное время
+    }
+    return count;
+}
+// классика
+//int getSumOfOddIndex (int[] arr)
+//{
+//    int count=0;
+//    for (int i=0;i<arr.Length;i++){
+//       if (i%2==1) count+=arr[i];
+//    }
+//    return count;
+//}
+
+
+
+
+
+
+
 
 //Создание массива
 int[] GetArray(int size,int min,int max)
