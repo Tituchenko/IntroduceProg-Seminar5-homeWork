4d7d975586a5e89fdbd0f9314fa0cb713326c0a4
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-29 21:48:16 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+//Создание массива
+int[] GetArray(int size,int min,int max)
+{
+    int[] res= new int[size];
+    for (int i=0;i<size;i++){
+        res[i]=new Random().Next(min,max+1);
+    }
+    return res;
+}
+
+//Вывод массива
+void printArray (int[] arr)
+{
+    Console.WriteLine ($"[{String.Join(",",arr)}]");
+}
