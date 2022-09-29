8145f3b2ece1b4c705edd87c89a623e3fce2d575
Ilya Tituchenko <titu4enko@mail.ru>
2022-09-29 23:07:50 +0300

    Ex 38 solved get delta max min

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ int getEvenElements (int[] arr)
 }
 */
 
-
+/*
 //Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
 
 //[3, 7, 23, 12] -> 19
@@ -47,12 +47,28 @@ int getSumOfOddIndex (int[] arr)
 //    }
 //    return count;
 //}
+*/
+
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+//[3 7 22 2 78] -> 76
+
+int[] arr=GetArrayFromString ();
+printArray(arr);
+Console.WriteLine ($" -> {getMaxMinDelta(arr)}");
 
 
+int getMaxMinDelta (int[] arr)
+{
+    int max=arr[0];
+    int min=arr[0];
+    for (int i=1;i<arr.Length;i++){ // [0] бессмыслено проверять
+       if (max<arr[i]) max=arr[i];
+       if (min>arr[i]) min=arr[i];
+    }
+    return max-min;
 
-
-
-
+}
 
 
 //Создание массива
@@ -65,20 +81,30 @@ int[] GetArray(int size,int min,int max)
     return res;
 }
 
-/*
+
 //Ввод масива через терминал
 int[] GetArrayFromString()
 {
-    Console.Write ("Введите массив элементов через пробел:");
+    Console.Write ("Введите массив элементов через пробел (или просто Enter - 7 случайных чисел от -100 до 100):");
     string s= Console.ReadLine();
-    string[] tempStringArray=s.Split(" ");
-    int[] res= new int[tempStringArray.Length];
-    for (int i=0;i<tempStringArray.Length;i++){
-        res[i]=int.Parse(tempStringArray[i]);
+    if (s!="")
+    {
+        string[] tempStringArray=s.Split(" ");
+        int[] res= new int[tempStringArray.Length];
+        for (int i=0;i<tempStringArray.Length;i++)
+        {
+            res[i]=int.Parse(tempStringArray[i]);
+        }
+        return res;// почему то один return в конце за if ругается, что переменной нет, хотя она в if создастся точно (баг c#)
+    } 
+    else 
+    {
+        int[] res=GetArray(7,-100,100);
+        return res;
     }
-    return res;
+    
 }
-*/
+
 //Вывод массива
 void printArray (int[] arr)
 {
